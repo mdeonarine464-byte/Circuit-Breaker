06c68e9d453a82b5c1eb5b806d884fe74cabfad5
mdeonarine464-byte <mdeonarine464@gmail.com>
2026-05-20 11:02:59 -0500

    Add main class and game initialization

---
+++ MyProgram.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class MyProgram {
+    public static void main(String[] args) {
+        Game game = new Game(false); // Start as standard game 
+        game.start();
+    }
+}
